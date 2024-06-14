b629b5b28a36f5fa6e102153e14e7e9bd9fa71ba
Harshawardhan Kamble <kambleharsh08@outlook.com>
2024-06-14 10:28:05 +0530

    backend

---
+++ backend/src/routes/blogRouter.ts
@@ -0,0 +1,107 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { verify } from "hono/jwt";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+    prisma: any;
+  };
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const [, token] = authHeader.split(" ");
+  const user = await verify(token, c.env.JWT_SECRET);
+  if (user) {
+    c.set("userId", user.id as string);
+    await next();
+  } else {
+    c.status(403);
+    return c.json({
+      msg: "You're not Logged in",
+    });
+  }
+});
+blogRouter.post("/", async (c) => {
+  try {
+    const prisma = c.get("prisma");
+    const userId = c.get("userId");
+    const { title, content } = await c.req.json();
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId: userId,
+        published:true
+      },
+    });
+    return c.json({
+      post: post,
+      id:post.id
+    });
+  } catch (error) {
+    console.log(error);
+    return c.json({
+      msg: "Something went wrong",
+    });
+  }
+});
+
+blogRouter.put("/", async (c) => {
+try {
+  const prisma = c.get("prisma");
+  const userId = c.get("userId");
+  const { id, title, content } = await c.req.json();
+  const update=await prisma.post.update({
+    where: {
+      id,
+      authorId: userId,
+    },
+    data: {
+      title,
+      content,
+    },
+  });
+  return c.json({ msg: "Blog Updated Succesfully!" });
+} catch (error) {
+  console.log(error)
+  c.json({
+  msg:"Something went Wrong"
+  })
+}
+});
+// pagination needs to be added
+blogRouter.get("/bulk", async (c) => {
+  const prisma = c.get("prisma");
+  const posts = await prisma.post.findMany();
+  console.log(posts)
+  return c.json({ posts });
+});
+blogRouter.get("/:id", async (c) => {
+  const prisma = c.get("prisma");
+  try {
+    const id = c.req.param("id");
+    const postById = await prisma.post.findFirst({
+      where: {
+        id,
+      },
+    });
+    return c.json({
+      postById,
+    });
+  } catch (error) {
+    console.log(error);
+    c.status(411);
+    return c.json({
+      message: "Error while fetching post",
+    });
+  }
+});
+
+
---
+++ backend/src/routes/userRouter.ts
@@ -0,0 +1,66 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { hashPassword } from "../utils/pwdUtils";
+import { sign, verify } from 'hono/jwt'
+export const userRouter=new Hono<{
+    Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+    },
+    Variables:{
+      prisma:any
+    };
+  }>()
+userRouter.post("/signup", async (c) => {
+    try {
+       const prisma=c.get('prisma');
+      const body=await c.req.json()
+      const {email,password,name}=body
+      const hashPwd=await hashPassword(password)
+      const user=await prisma.user.create({
+        data:{
+          email,
+          password:hashPwd,
+          name
+        }}
+      )
+      const token=await sign({id:user.id},c.env.JWT_SECRET)
+      return c.json({
+        msg:"Signed up Successfully",
+        token
+      });
+    } catch (error) {
+      console.log(error)
+      return c.json({
+        msg:"Something Went wrong!!!!!"
+      })
+    }
+  });
+  
+  userRouter.post("/signin", async (c) => {
+    try {
+       const prisma=c.get('prisma');
+      const body=await c.req.json()
+      const {email,password}=body
+      const hashPwd=await hashPassword(password)
+      const existingUser=await prisma.user.findFirst({
+        where:{
+          email,
+          password:hashPwd
+        }
+      })
+      if (existingUser){
+        const token=await sign({id:existingUser.id},c.env.JWT_SECRET)
+        return c.json({sucess:true,msg:"Signed in Successfully",token});
+      }
+      else{
+        return c.json({sucess:false,msg:" Incorrect Credentials"});
+      }
+    } catch (error) {
+      console.log(error)
+      return c.json({
+        msg:"Something went wrong"
+      })
+    }
+  });
