5fcc39edb1f5d7766e3f6015da2d65278f1486bb
Harshawardhan Kamble <kambleharsh08@outlook.com>
2024-06-26 23:22:03 +0530

    updated

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,12 +1,12 @@
 import z from "zod"
 export const signupInput=z.object({
     email:z.string().email(),
-    password:z.string().min(6),
+    password:z.string().min(8),
     name:z.string().optional()
 })
 export const signinInput=z.object({
     email:z.string().email(),
-    password:z.string()
+    password:z.string().min(8)
 })
 export const createBlogInput = z.object({
     title: z.string(),
